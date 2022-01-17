a9505c13ec68c681d91c3c86ccc08ed77603a717
dingkui13452903663@163.com <13452903663@163.com>
2022-01-17 09:56:52 +0800

    server https

--- server/index.js
+++ server/index.js
@@ -1,18 +1,20 @@
-const { createServer } = require('http')
+const { createServer } = require("http");
 
 createServer(function (req, res) {
   setTimeout(() => {
     res.writeHead(200, {
-      'Content-type' : 'application/json; charset=utf-8',
-      "Access-Control-Allow-Origin": "*"
+      "Content-type": "application/json; charset=utf-8",
+      "Access-Control-Allow-Origin": "*",
     });
-    res.write(JSON.stringify({
-      errno: 0,
-      message: '',
-      data: true
-    }));
+    res.write(
+      JSON.stringify({
+        errno: 0,
+        message: "",
+        data: true,
+      })
+    );
     res.end();
-  }, 2000)
-}).listen(8000, () => {
-  console.log('server in listen on 8000')
-})
+  }, 2000);
+}).listen(3000, () => {
+  console.log("server in listen on 3000");
+});
